7dfba268b1a7f06233e9f23b16cc21779018d7dd
chaosRobe <dominik.panasz@outlook.com>
2026-05-08 10:18:34 +0200

    5,6

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { AboutComponent } from './about/about.component';
 import { ListaSokowComponent } from './soki/lista-sokow/lista-sokow.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'soki', component: ListaSokowComponent },
   { path: 'about', component: AboutComponent },
   { path: '**', redirectTo: '' }
